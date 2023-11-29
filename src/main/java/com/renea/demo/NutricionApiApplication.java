c09ae05504f1b05580bf9b33939798ccf35c3bae
renea <reneadonayfrancoperez@gmail.com>
2023-11-29 14:46:37 -0500

    first Commit

---
+++ src/main/java/com/renea/demo/NutricionApiApplication.java
@@ -0,0 +1,13 @@
+package com.renea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renea/demo/NutricionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
